1cdfd6d41e99656c34e474dba049ecf71ee169df
gidroca <121635084+gidroca@users.noreply.github.com>
2022-12-29 21:06:31 +0300

    Add files via upload

---
+++ ичичочи.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Мой твитч канал</title>
+</head>
+<body bgcolor="555555">
+
+</body>
+</html>
+<font color="green" face="Tahoma"><h1><center>NEKTO3540 БОТ <center></h1></font>
+<font size="9" color="red" face="Tahoma"><center>ИЧО<center></font>
+<hr color="yellow">
+<h1>Скоро вставлю ссылку!</h1>
+<br>
+<img src="https://i.pinimg.com/736x/26/f5/e2/26f5e2fdc8ab53d08596a7d1144d86e7.jpg" width="100" height="100">
+<br><br><br>
+<table border="4">
+<tr>
+<td><h3>ичо</h3></td>
+<td><h3>nicho</h3></td>
+</tr>
+
+<tr>
+<td><h3>лес</h3></td>	
+<td><h3>поле</h3></td>
+</tr>
+</table>
+
